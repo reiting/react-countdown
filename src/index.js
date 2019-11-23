8467ae8b45973b327abb6aa49bd4ddc5753cf766
Rachel Eiting <reiting1@gmail.com>
2019-11-23 15:22:32 -0600

    Basic countdown function working

--- src/index.js
+++ src/index.js
@@ -9,6 +9,27 @@ class Countdown extends React.Component {
         minutes: undefined,
         seconds: undefined
     };
+
+    componentDidMount() {
+        this.interval = setInterval(()=> {
+            const { timeTillDate, timeFormat } = this.props;
+            const then = moment(timeTillDate, timeFormat);
+            const now = moment();
+            const countdown = moment(then - now);
+            const days = countdown.format('D');
+            const hours = countdown.format('HH');
+            const minutes = countdown.format('mm');
+            const seconds = countdown.format('ss');
+            this.setState({ days, hours, minutes, seconds });
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
+    }
+    
     render() {
         const { days, hours, minutes, seconds } = this.state;
         return (
