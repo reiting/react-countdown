03d56541b063ab3a182dd7b5eaa2b36f1d2081dc
Rachel Eiting <reiting1@gmail.com>
2019-11-23 15:10:45 -0600

    Created files and set up basic page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import moment from 'moment';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Countdown extends React.Component {
+    state = {
+        days: undefined,
+        hours: undefined,
+        minutes: undefined,
+        seconds: undefined
+    };
+    render() {
+        const { days, hours, minutes, seconds } = this.state;
+        return (
+            <div>
+                <h1>Countdown</h1>
+                <div className='countdown-wrapper'>
+                    <div className='countdown-item'>
+                        {days}
+                        <span>days</span>
+                    </div>
+                    <div className='countdown-item'>
+                        {hours}
+                        <span>hours</span>
+                    </div>
+                    <div className='countdown-item'>
+                        {minutes}
+                        <span>minutes</span>
+                    </div>
+                    <div className='countdown-item'>
+                        {seconds}
+                        <span>seconds</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ========================================
+
+ReactDOM.render(
+    <Countdown />,
+    document.getElementById('root')
+);
